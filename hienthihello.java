7c069008258e604d579ff781e51547c5d851c750
unknown <nguyenduytai945@gmail.com>
2019-06-04 08:28:43 +0700

    add

---
+++ hienthihello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class hienthihello {
+    public static void main(String[] args) {
+        Scanner hello1 = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = hello1.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
